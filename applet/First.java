32f3502f110cc9c64c15aca2373ece41c701648f
Rashmi Vernekar <109080802+RashmivernekarN@users.noreply.github.com>
2023-06-16 12:52:09 +0530

    Add files via upload

---
+++ applet/First.java
@@ -0,0 +1,13 @@
+
+  
+import java.applet.Applet;  
+import java.awt.Graphics;  
+
+public class First extends Applet
+{    
+public void paint(Graphics g)
+{  
+g.drawString("welcome",150,150);  
+}  
+  
+}  
